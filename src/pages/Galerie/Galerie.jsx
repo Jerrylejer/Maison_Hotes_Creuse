183d1c90dae03ae5fbc5791c46fe2e9948860cba
Jerrylejer <81814979+Jerrylejer@users.noreply.github.com>
2022-10-28 16:56:41 +0200

    Amélioration html section galerie avec figure & figcaption +  margib-top & bottom ajustés en version mobile

--- src/pages/Galerie/Galerie.jsx
+++ src/pages/Galerie/Galerie.jsx
@@ -25,14 +25,14 @@ import cuisine1 from '../../assets/images/cuisine1_pf-min.jpg';
 import cuisine2 from '../../assets/images/cuisine2_pf-min.jpg';
 import salon1 from '../../assets/images/salon1_pf-min.jpg';
 import salon2 from '../../assets/images/salon2_pf-min.jpg';
-import salon3 from '../../assets/images/salon3_pf-min.jpg';
+// import salon3 from '../../assets/images/salon3_pf-min.jpg';
 import terrasse1 from '../../assets/images/terrasse1_pf-min.jpg';
 import terrasse2 from '../../assets/images/terrasse2_pf-min.jpg';
 import piscine1 from '../../assets/images/piscine1_pf-min.jpg';
 import piscine2 from '../../assets/images/piscine2_pf-min.jpg';
 import piscine3 from '../../assets/images/piscine3_pf-min.jpg';
 import piscine4 from '../../assets/images/piscine4_pf-min.jpg';
-import tours from '../../assets/images/tours_pf-min.jpg';
+// import tours from '../../assets/images/tours_pf-min.jpg';
 import jardin from '../../assets/images/jardin_pf-min.jpg';
 import cours from '../../assets/images/cours_pf-min.jpg';
 import escaliers from '../../assets/images/escaliers_pf-min.jpg';
@@ -71,92 +71,147 @@ const Galerie = () => {
                 }}
             >
                 <SwiperSlide>
-                    <img className='photo' src={maison} alt='maison' />
-                    <div className='text text--big'>Vue de la terrasse</div>
+                    <figure>
+                        <img className='photo' src={maison} alt='maison' />
+                        <figcaption className='text text--big'>
+                            La Maison d'Odile
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img
-                        className='photo'
-                        src={chambreRouge}
-                        alt='chambre_Rouge'
-                    />
-                    <div className='text'>Chambre Rouge</div>
+                    <figure>
+                        <img
+                            className='photo'
+                            src={chambreRouge}
+                            alt='chambre_Rouge'
+                        />
+                        <figcaption className='text'>
+                            La chambre rouge
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img
-                        className='photo'
-                        src={chambreBeige}
-                        alt='chambre_Beige'
-                    />
-                    <div className='text text--big'>Chambre Beige</div>
+                    <figure>
+                        <img
+                            className='photo'
+                            src={chambreBeige}
+                            alt='chambre_Beige'
+                        />
+                        <figcaption className='text text--big'>
+                            La chambre beige
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={cuisine1} alt='cuisine' />
-                    <div className='text'>Cuisine</div>
+                    <figure>
+                        <img className='photo' src={cuisine1} alt='cuisine' />
+                        <figcaption className='text'>
+                            L'authentique cuisine
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={cuisine2} alt='buffet_pdj' />
-                    <div className='text'>Buffet petit-déjeuner</div>
+                    <figure>
+                        <img
+                            className='photo'
+                            src={cuisine2}
+                            alt='buffet_pdj'
+                        />
+                        <figcaption className='text'>
+                            Les essentiels au petit-déjeuner !
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={salon1} alt='salon1' />
-                    <div className='text'>Petit-salon</div>
+                    <figure>
+                        <img className='photo' src={salon1} alt='salon1' />
+                        <figcaption className='text'>Le salon</figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img
-                        className='photo photo--vert'
-                        src={salon3}
-                        alt='salon3'
-                    />
-                    <div className='text'>Petit-salon</div>
+                    <figure>
+                        <img className='photo' src={salon2} alt='salon2' />
+                        <figcaption className='text'>
+                            Notre salle-à-manger
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={salon2} alt='salon2' />
-                    <div className='text'>Salle-à-manger</div>
+                    <figure>
+                        <img
+                            className='photo'
+                            src={terrasse1}
+                            alt='terrasse1'
+                        />
+                        <figcaption className='text'>Notre terrasse de droite ...</figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={terrasse1} alt='terrasse1' />
-                    <div className='text'>Terrasse</div>
+                    <figure>
+                        <img
+                            className='photo'
+                            src={terrasse2}
+                            alt='terrasse2'
+                        />
+                        <figcaption className='text'>... à gauche !</figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={terrasse2} alt='terrasse2' />
-                    <div className='text'>Terrasse</div>
+                    <figure>
+                        <img className='photo' src={piscine1} alt='piscine1' />
+                        <figcaption className='text'>
+                            Notre piscine et ses transats
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img
-                        className='photo photo--vert'
-                        src={tours}
-                        alt='tours'
-                    />
-                    <div className='text'>Maison</div>
+                    <figure>
+                        <img className='photo' src={piscine2} alt='piscine2' />
+                        <figcaption className='text'>
+                            Grande pour se baigner en famille ...
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={piscine1} alt='piscine1' />
-                    <div className='text'>Piscine et transats</div>
+                    <figure>
+                        <img className='photo' src={piscine3} alt='piscine3' />
+                        <figcaption className='text'>
+                            {' '}
+                            ... et sous des regards protecteurs
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={piscine2} alt='piscine2' />
-                    <div className='text'>Autre vue piscine</div>
+                    <figure>
+                        <img className='photo' src={piscine4} alt='piscine4' />
+                        <figcaption className='text'>
+                            Piscine de 9 x 4 mètres
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={piscine3} alt='piscine3' />
-                    <div className='text'>Autre vue piscine</div>
+                    <figure>
+                        <img className='photo' src={jardin} alt='jardin' />
+                        <figcaption className='text'>
+                            Vue sur le jardin
+                        </figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={piscine4} alt='piscine4' />
-                    <div className='text'>Autre vue piscine</div>
+                    <figure>
+                        <img
+                            className='photo'
+                            src={escaliers}
+                            alt='escaliers'
+                        />
+                        <figcaption className='text'>Accès terrasse</figcaption>
+                    </figure>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img className='photo' src={jardin} alt='jardin' />
-                    <div className='text'>Vue sur le jardin</div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='photo' src={escaliers} alt='escaliers' />
-                    <div className='text'>Accès terrasse</div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img className='photo' src={cours} alt='cours' />
-                    <div className='text'>Cours et parking</div>
+                    <figure>
+                        <img className='photo' src={cours} alt='cours' />
+                        <figcaption className='text'>Cour et parking</figcaption>
+                    </figure>
                 </SwiperSlide>
             </Swiper>
             <Footer />
